c129f0ceb7a2944da0f59d08dbd0a6ad4998a7a6
HalaszIza <halasziza@gmail.com>
2024-04-11 12:56:04 +0200

    lab-07_hf

---
+++ lab-07_hf/lab-07_hf.cpp
@@ -0,0 +1,21 @@
+#include <iostream> 
+#include <fstream> 
+#include <string> 
+#include <ctime>
+using namespace std;
+
+struct workors
+{
+	string name;
+	int birth_y;
+	int work_h;
+	int money;
+};
+
+
+
+
+int main()
+{
+
+}
